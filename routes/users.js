c7cc7d31c71e0cc7161c068744f4d1a898933c9a
Karthik Meda <karthmeda@gmail.com>
2017-01-26 19:08:37 -0500

    fazbook up till delete

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models/index');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+// router.get('/', function(req, res, next) {
+//   res.render('users/index', { title: 'fazbook' });
+// });
+
+// New users
+router.get('/new', function(req, res, next) {
+  res.render('users/new', { title: 'new users' });
 });
+
+router.get('/', function(req, res, next) {
+  models.User.findAll({}).then(function(users) {
+    res.render('users/index', {
+      title: 'fazbook',
+      users: users
+    });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  models.User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  }).then(function() {
+    res.redirect('/users')
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  models.User.destroy({
+    where: { id: req.params.id }
+  }).then(function(user) {
+    res.redirect('/users');
+  });
+});
+
 
 module.exports = router;
